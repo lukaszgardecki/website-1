d0ce367458d5aeb9ab3939da5bec82c6819ae137
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-11 21:23:55 +0100

    Unlock access to H2 console in SecurityConfig

--- src/main/java/com/example/app/config/SecurityConfig.java
+++ src/main/java/com/example/app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.app.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +11,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // to show H2 console:
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()))
+                .headers().frameOptions().sameOrigin();
+
         return http.authorizeHttpRequests(requests -> requests
                         .requestMatchers("/").permitAll()
-                        .requestMatchers("../static/styles/**", "../static/scripts/**", "..static/img/**").permitAll()
+                        .requestMatchers("/styles/**", "/scripts/**", "/img/**").permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .anyRequest().permitAll())
                 .build();
     }
