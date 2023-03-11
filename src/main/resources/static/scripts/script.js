f786b2728b616f26e873b66d9586fc01e217eae7
lukaszgardecki <105553857+lukaszgardecki@users.noreply.github.com>
2023-03-11 20:00:16 +0100

    Add reset password form (#2)
    Changes in index.html, style.css, script.js.
    Add reset password form to index.html and add new styles
    Add evenListener to "Forgot password?" link.

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,6 +1,8 @@
 const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
+const loginLinkForgot = document.querySelector('.login-link-forgot');
 const registerLink = document.querySelector('.register-link');
+const forgotLink = document.querySelector('.forgot-link');
 
 registerLink.addEventListener('click', () => {
     logregBox.classList.add('active');
@@ -8,4 +10,12 @@ registerLink.addEventListener('click', () => {
 
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
+});
+
+forgotLink.addEventListener('click', () => {
+    logregBox.classList.add('active2');
+});
+
+loginLinkForgot.addEventListener('click', () => {
+    logregBox.classList.remove('active2');
 });
