a1badc70fc2b82f03166c9a4e55c7aad5a030679
Łukasz Gardecki <lukas.g1804@gmail.com>
2023-03-17 21:15:31 +0100

    Create GET userPage() : /user

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -3,13 +3,12 @@ package com.example.app.controller;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
-
-    @PostMapping("/user")
+    @GetMapping("/user")
     public String userPage(Model model, Authentication authentication) {
         String name = authentication.getName();
         model.addAttribute("name", name);
