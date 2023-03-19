5146e667211496a5b10562c5f918791a9267398a
lukaszgardecki <105553857+lukaszgardecki@users.noreply.github.com>
2023-03-19 18:30:13 +0100

    PR - Add notification bar (#5)
    * Add notification bar template
    
    * Add disappearing bar
    Adding disappearing bar contains notifications or logout message.

--- src/main/java/com/example/app/controller/HomeController.java
+++ src/main/java/com/example/app/controller/HomeController.java
@@ -1,19 +1,14 @@
 package com.example.app.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    public static final String NOTIFICATION_ATTRIBUTE = "notification";
 
     @GetMapping("/")
-    String home(Model model, Authentication authentication) {
-        if (authentication != null) {
-            String name = authentication.getName();
-            model.addAttribute("name", name);
-        }
+    String home() {
         return "index";
     }
 }
--- src/main/java/com/example/app/controller/LoginController.java
+++ src/main/java/com/example/app/controller/LoginController.java
@@ -1,13 +1,19 @@
 package com.example.app.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
-    @PostMapping("/success")
-    public String successLogin() {
+    @PostMapping("/success-login")
+    public String successLogin(RedirectAttributes redirectAttributes, Authentication authentication) {
+        redirectAttributes.addFlashAttribute(
+                HomeController.NOTIFICATION_ATTRIBUTE,
+                "Welcome %s!".formatted(authentication.getName())
+        );
         return "redirect:/";
     }
 }
